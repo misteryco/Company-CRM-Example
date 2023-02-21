df2a2b833739d24d6dfe0ddcd949851d451817f8
misteryco <jordan.v.dimov@gmail.com>
2023-02-21 10:21:58 +0200

    cloudinary added

--- crm_for_companies/api_companies/models.py
+++ crm_for_companies/api_companies/models.py
@@ -1,3 +1,32 @@
+from django.core.validators import MinLengthValidator
 from django.db import models
 
-# Create your models here.
+
+class Company(models.Model):
+    NAME_MAX_LEN = 30
+    NAME_MIN_LEN = 3
+    DESCRIPTION_MIN_LENGTH = 10
+    DESCRIPTION_MAX_LENGTH = 300
+
+    name = models.CharField(
+        max_length=NAME_MAX_LEN,
+        validators=(
+            MinLengthValidator(NAME_MIN_LEN),
+        ),
+        null=False,
+        blank=False,
+    )
+
+    description = models.TextField(
+        max_length=DESCRIPTION_MAX_LENGTH,
+        validators=(
+            MinLengthValidator(DESCRIPTION_MIN_LENGTH),
+        ),
+        null=False,
+        blank=False,
+
+    )
+    #
+    # logo = models.ImageField(
+    #
+    # )
