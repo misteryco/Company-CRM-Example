8621211a830e91a50bc1f3c8884bedfa574a4a96
misteryco <jordan.v.dimov@gmail.com>
2023-02-22 20:02:24 +0200

    delete company url

--- crm_for_companies/api_companies/urls.py
+++ crm_for_companies/api_companies/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from crm_for_companies.api_companies.views import CompanyListApiView, CustomCompanyListApiView, \
-    CustomCreateCompanyApiView
+    CustomCreateCompanyApiView, CustomDeleteCompanyApiView
 
 urlpatterns = (
     path('', CompanyListApiView.as_view(), name='api list company'),
     path('custom/', CustomCompanyListApiView.as_view(), name='api custom list company'),
     path('custom/create/', CustomCreateCompanyApiView.as_view(), name='api custom create company'),
+    path('custom/delete/<int:pk>/', CustomDeleteCompanyApiView.as_view(), name='api custom delete company'),
 )
