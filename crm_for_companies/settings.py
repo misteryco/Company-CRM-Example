d165eaeec9ee53bde129345dc38533abac3a267b
misteryco <jordan.v.dimov@gmail.com>
2023-02-24 14:31:32 +0200

    added rest default permission classes

--- crm_for_companies/settings.py
+++ crm_for_companies/settings.py
@@ -136,9 +136,13 @@ cloudinary.config(
     api_secret=env('CLOUDINARY_API_SECRET'),
 )
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
