8617909c1c09a487ac6af706adabdfec1d8ebf86
YD <jordan.v.dimov@gmail.com>
2023-05-19 17:44:13 +0300

    form to test CSRF tokens

--- crm_for_companies/web/models.py
+++ crm_for_companies/web/models.py
@@ -1,9 +1,3 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import User
-from django.db import models
 
 UserModel = get_user_model()
-
-
-class GuiPage:
-    pass
