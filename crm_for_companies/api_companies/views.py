d4a5654c170c6cab3d4eb692863d3b7d2058a16b
YD <jordan.v.dimov@gmail.com>
2023-06-23 16:52:48 +0300

    swagger config added

--- crm_for_companies/api_companies/views.py
+++ crm_for_companies/api_companies/views.py
@@ -1,19 +1,23 @@
 import cloudinary.uploader
 from django.shortcuts import get_object_or_404
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework import views as rest_basic_views
 from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import (SAFE_METHODS, BasePermission,
-                                        IsAuthenticated)
+from rest_framework.permissions import SAFE_METHODS, BasePermission, IsAuthenticated
 from rest_framework.response import Response
 
 from crm_for_companies.api_companies.models import Company, UserModel
 from crm_for_companies.api_companies.serializers import (
-    CompanyCreateSerializer, CompanySerializer, CompanySerializerWithEmployees)
+    CompanyCreateSerializer,
+    CompanySerializer,
+    CompanySerializerWithEmployees,
+)
 
 
 class IsCompanyOwner(BasePermission):
-    message = 'Only Owner can see company details.'
+    message = "Only Owner can see company details."
 
     def has_permission(self, request, view):
         # Check if the user is authenticated
@@ -22,8 +26,7 @@ class IsCompanyOwner(BasePermission):
         # Allow access for superusers
         if request.user.is_superuser:
             return True
-        # Allow access if the user is the owner
-        if request.method in SAFE_METHODS:
+        if request.method:
             return True
 
         return False
@@ -54,11 +57,13 @@ class CompanyListApiView(rest_basic_views.APIView):
     def get(self, request):
         serializer = self.serializer_class(self.get_queryset(), many=True)
 
-        return Response({'data': serializer.data, 'status': status.HTTP_200_OK},
-                        status=status.HTTP_200_OK, )
+        return Response(
+            {"data": serializer.data, "status": status.HTTP_200_OK},
+            status=status.HTTP_200_OK,
+        )
 
     def get_queryset(self):
-        company_id = self.request.query_params.get('company_id')
+        company_id = self.request.query_params.get("company_id")
         queryset = self.queryset.all()
         if company_id:
             queryset = queryset.filter(id=company_id)
@@ -68,22 +73,46 @@ class CompanyListApiView(rest_basic_views.APIView):
 class CreateCompanyApiView(rest_basic_views.APIView):
     queryset = Company.objects.all()
 
+    # @swagger_auto_schema(
+    #     request_body=openapi.Schema(
+    #         type=openapi.TYPE_OBJECT,
+    #         properties={
+    #             'owner': openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Items(type=openapi.TYPE_INTEGER)),
+    #             'name': openapi.Schema(type=openapi.TYPE_STRING),
+    #             'description': openapi.Schema(type=openapi.TYPE_STRING),
+    #             'logo': openapi.Schema(type=openapi.TYPE_STRING),
+    #         },
+    #         required=['owner', 'name', 'description', 'logo'],
+    #     )
+    # )
+    @swagger_auto_schema(
+        request_body=CompanyCreateSerializer, responses={200: CompanyCreateSerializer}
+    )
     def post(self, request, *args, **kwargs):
-        serializer = CompanyCreateSerializer(data=request.data,
-                                             context={'request': request})
+        serializer = CompanyCreateSerializer(
+            data=request.data, context={"request": request}
+        )
         if serializer.is_valid(raise_exception=True):
-            file = request.data.get('logo')
+            file = request.data.get("logo")
             logo = cloudinary.uploader.upload(file)
-            serializer.validated_data['logo'] = logo['url']
+            serializer.validated_data["logo"] = logo["url"]
             serializer.save()
-            return Response({'data': serializer.data, 'errors': serializer.errors, 'status': status.HTTP_201_CREATED},
-                            status=status.HTTP_201_CREATED, )
+            return Response(
+                {
+                    "data": serializer.data,
+                    "errors": serializer.errors,
+                    "status": status.HTTP_201_CREATED,
+                },
+                status=status.HTTP_201_CREATED,
+            )
 
-        return Response({'errors': serializer.errors, 'status': status.HTTP_400_BAD_REQUEST})
+        return Response(
+            {"errors": serializer.errors, "status": status.HTTP_400_BAD_REQUEST}
+        )
 
 
 class DetailsCompanyApiView(rest_basic_views.APIView):
-    http_method_names = ['get', 'delete', 'put']
+    http_method_names = ["get", "delete", "put"]
     queryset = Company.objects.all()
     serializer_class = CompanySerializer
 
@@ -95,23 +124,50 @@ class DetailsCompanyApiView(rest_basic_views.APIView):
 
         return Response(instance.data)
 
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                "owner": openapi.Schema(
+                    type=openapi.TYPE_ARRAY,
+                    items=openapi.Items(type=openapi.TYPE_INTEGER),
+                ),
+                "name": openapi.Schema(type=openapi.TYPE_STRING),
+                "description": openapi.Schema(type=openapi.TYPE_STRING),
+                "logo": openapi.Schema(type=openapi.TYPE_STRING),
+            },
+            required=["owner", "name", "description", "logo"],
+        )
+    )
     def put(self, request, pk):
         instance = get_object_or_404(Company, pk=pk)
 
         serializer = self.serializer_class(instance, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response({'data': serializer.data, 'errors': serializer.errors, 'status': status.HTTP_200_OK},
-                            status=status.HTTP_200_OK, )
+            return Response(
+                {
+                    "data": serializer.data,
+                    "errors": serializer.errors,
+                    "status": status.HTTP_200_OK,
+                },
+                status=status.HTTP_200_OK,
+            )
 
-        return Response({'errors': serializer.errors, 'status': status.HTTP_400_BAD_REQUEST},
-                        status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            {"errors": serializer.errors, "status": status.HTTP_400_BAD_REQUEST},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
     def delete(self, request, pk):
         try:
             instance = Company.objects.get(pk=pk)
         except Company.DoesNotExist:
-            return Response({'status': status.HTTP_404_NOT_FOUND}, status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"status": status.HTTP_404_NOT_FOUND}, status=status.HTTP_404_NOT_FOUND
+            )
         # instance = self.queryset.filter(pk=pk).get()
         instance.delete()
-        return Response({'status': status.HTTP_204_NO_CONTENT}, status=status.HTTP_204_NO_CONTENT)
+        return Response(
+            {"status": status.HTTP_204_NO_CONTENT}, status=status.HTTP_204_NO_CONTENT
+        )
--- crm_for_companies/settings.py
+++ crm_for_companies/settings.py
@@ -9,12 +9,11 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-import environ
-import cloudinary
-
 import os
-
 from pathlib import Path
+
+import cloudinary
+import environ
 
 env = environ.Env()
 environ.Env.read_env()
@@ -26,70 +25,67 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ['*', 'localhost', '127.0.0.1', 'http://localhost:63342/']
+ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1", "http://localhost:63342/"]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
     # Third party apps:
     # DJANGO REST APP
-    'rest_framework',
+    "rest_framework",
     # REST token accounts (python manage.py migrate after this row :) )
-    'rest_framework.authtoken',
+    "rest_framework.authtoken",
     # Cloudinary
-    'cloudinary',
+    "cloudinary",
     # Django Celery Beat
-    'django_celery_beat',
-
+    "django_celery_beat",
+    "drf_yasg",
     # Application apps:
-    'crm_for_companies.api_companies',
-    'crm_for_companies.api_employees',
-
+    "crm_for_companies.api_companies",
+    "crm_for_companies.api_employees",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
     # next row is enabled after it was disabled for test purposes
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'crm_for_companies.urls'
+ROOT_URLCONF = "crm_for_companies.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'crm_for_companies.wsgi.application'
+WSGI_APPLICATION = "crm_for_companies.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -102,13 +98,13 @@ WSGI_APPLICATION = 'crm_for_companies.wsgi.application'
 # }
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'crm_tables',
-        'USER': env('DB_USERNAME'),
-        'PASSWORD': env('DB_PASSWORD'),
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "crm_tables",
+        "USER": env("DB_USERNAME"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
@@ -156,9 +152,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -168,20 +164,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 # STATIC_URL = 'static/' - original
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 # Following row is required for collectstatic command
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 cloudinary.config(
-    cloud_name=env('CLOUDINARY_CLOUD_NAME'),
-    api_key=env('CLOUDINARY_API_KEY'),
-    api_secret=env('CLOUDINARY_API_SECRET'),
+    cloud_name=env("CLOUDINARY_CLOUD_NAME"),
+    api_key=env("CLOUDINARY_API_KEY"),
+    api_secret=env("CLOUDINARY_API_SECRET"),
 )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # REST_FRAMEWORK = {
 #     'DEFAULT_PERMISSION_CLASSES': [
@@ -189,10 +185,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #     ]
 # }
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        # 'rest_framework.authentication.BasicAuthentication',
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.BasicAuthentication",
         # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        "rest_framework.authentication.TokenAuthentication",
         # 'rest_framework.permissions.IsAuthenticated',
     ],
 }
@@ -201,16 +197,23 @@ REST_FRAMEWORK = {
 # AUTH_USER_MODEL = 'User'
 
 # CELERY settings
-CELERY_BROKER_URL = 'pyamqp://guest@localhost//'
-CELERY_RESULT_BACKEND = 'db+postgresql://postgres:postgres@localhost/crm_tables'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
+CELERY_BROKER_URL = "pyamqp://guest@localhost//"
+CELERY_RESULT_BACKEND = "db+postgresql://postgres:postgres@localhost/crm_tables"
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+
+# Swagger settings
+SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+        "api_key": {"type": "apiKey", "in": "header", "name": "Authorization"}
+    },
+}
