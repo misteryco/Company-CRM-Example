dcf7ec936ec51b9ad9b6e9b1933a13c0fb66c961
YD <jordan.v.dimov@gmail.com>
2023-06-28 16:46:40 +0300

    refactored

--- crm_for_companies/api_companies/views.py
+++ crm_for_companies/api_companies/views.py
@@ -13,48 +13,10 @@ from crm_for_companies.api_companies.serializers import (
     CompanySerializer,
     CompanySerializerWithEmployees,
 )
-
-
-def get_first_user_pk():
-    user = UserModel.objects.all().first()
-
-    if user is not None:
-        return user.pk
-    else:
-        return None
-
-
-company_request_body_schema = openapi.Schema(
-    type=openapi.TYPE_OBJECT,
-    properties={
-        "owner": openapi.Schema(
-            type=openapi.TYPE_ARRAY,
-            items=openapi.Items(type=openapi.TYPE_INTEGER),
-        ),
-        "name": openapi.Schema(type=openapi.TYPE_STRING),
-        "description": openapi.Schema(type=openapi.TYPE_STRING),
-        "logo": openapi.Schema(type=openapi.TYPE_STRING),
-    },
-    required=["owner", "name", "description", "logo"],
-    example={
-        "name": "Example Company",
-        "description": "Some example company description",
-        "logo": "/home/yd/Screenshot-10.png",
-        "owner": [
-            get_first_user_pk(),
-        ],
-    },
+from crm_for_companies.core.companies_functions import (
+    company_request_body_schema,
+    details_company_api_views_manual_parameters,
 )
-
-details_company_api_views_manual_parameters = [
-    openapi.Parameter(
-        "id",
-        openapi.IN_PATH,
-        description="id of the company to edit",
-        type=openapi.TYPE_INTEGER,
-        example=123,
-    )
-]
 
 
 class IsCompanyOwner(BasePermission):
