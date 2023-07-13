b27f0d19f0819f5dbdd61d608bd94c713ee1823a
YD <jordan.v.dimov@gmail.com>
2023-07-13 18:49:00 +0300

    it works now with health check

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@
 celery -A crm_for_companies worker -l info -E &
 
 # Start Celery Beat
-#celery -A crm_for_companies beat -l info &
+celery -A crm_for_companies beat -l info &
 
 # Start Django
 python3 manage.py runserver 0.0.0.0:8000
