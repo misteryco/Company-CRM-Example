7ff50cc311ec373813be5af4046071afde918da0
YD <jordan.v.dimov@gmail.com>
2023-07-14 12:10:17 +0300

    docker-compose with env. variables

--- crm_for_companies/settings.py
+++ crm_for_companies/settings.py
@@ -31,9 +31,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+# DEBUG = env("DEBUG")
+DEBUG = True
 
-ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1", "http://localhost:63342/"]
+# ALLOWED_HOSTS = ["*", "localhost", "127.0.0.1", "http://localhost:63342/"]
+ALLOWED_HOSTS = [
+    "*",
+]
 
 # Application definition
 
